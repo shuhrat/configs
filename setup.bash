23fa510ac6285a33ccf45b47cc2af98c55afc724
Anton Rudeshko <rudeshko@yandex-team.ru>
2013-04-22 14:05:54 +0400

    Using extra configs

--- setup.bash
+++ setup.bash
@@ -2,3 +2,6 @@ for file in .{profile,gitconfig,vimrc,inputrc}; do
     ln -fs ~/configs/$file ~/$file
 done
 unset file
+
+# yandex configs
+git clone git@github.yandex-team.ru:rudeshko/ya-configs.git ~/ya-configs && ~/ya-configs/setup.bash
